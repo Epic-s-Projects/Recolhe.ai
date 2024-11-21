6ff8f467403674b3d12c00175099b8558d98b4d3
epicestudar <viniciusftza@gmail.com>
2024-11-21 16:01:52 -0300

    melhorando pagina de coleta

--- administrador/lib/home_coleta_page.dart
+++ administrador/lib/home_coleta_page.dart
@@ -4,108 +4,119 @@ class HomeColetaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Header
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Olá, João!',
-                  style: TextStyle(
-                    color: Color(0xFF052E05),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                CircleAvatar(
-                  backgroundColor: Colors.grey[300],
-                  radius: 18,
-                  child: Icon(Icons.person, color: Colors.black),
-                ),
-              ],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFF4D6A5), // Cor superior
+              Color(0xFFF4A261), // Cor inferior
+            ],
           ),
-
-          // Calendar
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildCalendarItem('S', Color(0xFF5D6DFF)),
-                _buildCalendarItem('T', Color(0xFF109410)),
-                _buildCalendarItem('Q', Color(0xFFC59A64)),
-                _buildCalendarItem('Q', Color(0xFF109410)),
-                _buildCalendarItem('S', Color(0xFF109410)),
-                _buildCalendarItem('S', Color(0xFF109410)),
-                _buildCalendarItem('D', Color(0xFF109410)),
-              ],
-            ),
-          ),
-
-          // Address card
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
+        ),
+        child: Column(
+          children: [
+            // Header
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    elevation: 2,
-                    child: ListTile(
-                      leading: Container(
-                        width: 48,
-                        height: 48,
-                        color: Colors.grey[300], // Placeholder for the image
-                        child: Icon(Icons.image, color: Colors.grey),
-                      ),
-                      title: Text(
-                        'CASA',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text('Endereço\nPessoa'),
-                      trailing: Icon(Icons.close, color: Colors.black),
+                  Text(
+                    'Olá, João!',
+                    style: TextStyle(
+                      color: Color(0xFF052E05),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Spacer(),
-                  // Bottom Button
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF1A1D10),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 32, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.delete, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          'Realize sua coleta',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
+                  CircleAvatar(
+                    backgroundColor: Colors.grey[300],
+                    radius: 18,
+                    child: Icon(Icons.person, color: Colors.black),
                   ),
-                  SizedBox(height: 16),
                 ],
               ),
             ),
-          ),
-        ],
+
+            // Calendar
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildCalendarItem('S', Color(0xFF5D6DFF)),
+                  _buildCalendarItem('T', Color(0xFF109410)),
+                  _buildCalendarItem('Q', Color(0xFFC59A64)),
+                  _buildCalendarItem('Q', Color(0xFF109410)),
+                  _buildCalendarItem('S', Color(0xFF109410)),
+                  _buildCalendarItem('S', Color(0xFF109410)),
+                  _buildCalendarItem('D', Color(0xFF109410)),
+                ],
+              ),
+            ),
+
+            // Address card
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      elevation: 2,
+                      child: ListTile(
+                        leading: Container(
+                          width: 48,
+                          height: 48,
+                          color: Colors.grey[300], // Placeholder for the image
+                          child: Icon(Icons.image, color: Colors.grey),
+                        ),
+                        title: Text(
+                          'CASA',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text('Endereço\nPessoa'),
+                        trailing: Icon(Icons.close, color: Colors.black),
+                      ),
+                    ),
+                    Spacer(),
+                    // Bottom Button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF1A1D10),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 32, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.delete, color: Colors.white),
+                          SizedBox(width: 8),
+                          Text(
+                            'Realize sua coleta',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
