2374d8f440508f081f8d20b86b4cc095dff627c1
epicestudar <viniciusftza@gmail.com>
2024-11-22 15:58:16 -0300

    modificando background de fundo do aplicativo

--- administrador/lib/area_coleta_page.dart
+++ administrador/lib/area_coleta_page.dart
@@ -23,111 +23,101 @@ class _AreaColetaPageState extends State<AreaColetaPage> {
       ),
       body: Stack(
         children: [
-          // Fundo da página com gradiente
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xFFF4D6A5), Color(0xFFF4A261)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
+          // Imagem de fundo
+          Positioned.fill(
+            child: Image.asset(
+              'assets/fundoHome.png', // Caminho da imagem de fundo
+              fit: BoxFit.cover,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 50),
-                  // Header com saudação e avatar
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Olá, João!',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/avatar.png'),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 50),
-                  if (!showMapCard) ...[
-                    // Exibição inicial antes de "Ver Mapa"
-                    const Center(
-                      child: Text(
-                        'Aguardando entrada em área \nde coleta!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
+          ),
+          // Conteúdo da página
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                // Header com saudação e avatar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Olá, João!',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
                       ),
                     ),
-                    const SizedBox(height: 50),
-                    Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            colors: [Color(0xFF109410), Color(0xFF1AE91A)],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                          ),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              showMapCard = true; // Altera o estado para exibir o card
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.transparent,
-                            shadowColor: Colors.transparent,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 50, vertical: 15),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: const [
-                              Text(
-                                'Ver Mapa',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              SizedBox(width: 8),
-                              Icon(
-                                Icons.location_pin,
-                                color: Colors.white,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/avatar.png'),
                     ),
                   ],
-                  const Spacer(),
-                  // Imagem inferior
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Image.asset(
-                      'assets/folhas.png', // Substitua pelo caminho correto da sua imagem
-                      fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 50),
+                if (!showMapCard) ...[
+                  // Exibição inicial antes de "Ver Mapa"
+                  const Center(
+                    child: Text(
+                      'Aguardando entrada em área \nde coleta!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [Color(0xFF109410), Color(0xFF1AE91A)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            showMapCard = true; // Altera o estado para exibir o card
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const [
+                            Text(
+                              'Ver Mapa',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Icon(
+                              Icons.location_pin,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
-              ),
+                const Spacer(),
+              ],
             ),
           ),
           if (showMapCard)
--- administrador/lib/home_adm_page.dart
+++ administrador/lib/home_adm_page.dart
@@ -29,110 +29,105 @@ class HomeAdmPage extends StatelessWidget {
         selectedItemColor: Colors.green,
         unselectedItemColor: Colors.grey,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFFF4D6A5), Color(0xFFF4A261)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: Stack(
+        children: [
+          // Imagem de fundo
+          Positioned.fill(
+            child: Image.asset(
+              'assets/fundoHome.png',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Olá, João!',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
-                  ),
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/avatar.png'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 80),
-              Center(
-                child: Column(
+          // Conteúdo da página
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    // Botão "Iniciar Coleta"
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navegar para a página de área de coleta
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AreaColetaPage(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.black,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 15,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: const Text(
-                        'Iniciar coleta',
-                        style: TextStyle(fontSize: 18),
+                    const Text(
+                      'Olá, João!',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    // Botão "Ver Itens"
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navegar para a página "Ver Itens"
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomeColetaPage(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.black,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 15,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: const Text(
-                        'Ver Itens',
-                        style: TextStyle(fontSize: 18),
-                      ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/avatar.png'),
                     ),
                   ],
                 ),
-              ),
-              const Spacer(),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Image.asset(
-                  'assets/folhas.png', // Substitua pelo caminho correto
-                  fit: BoxFit.cover,
+                const SizedBox(height: 80),
+                Center(
+                  child: Column(
+                    children: [
+                      // Botão "Iniciar Coleta"
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navegar para a página de área de coleta
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AreaColetaPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 15,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Iniciar coleta',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Botão "Ver Itens"
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navegar para a página "Ver Itens"
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeColetaPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 15,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Ver Itens',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
